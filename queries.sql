054d8653034f622144382f92f8f9b1490fa25cdd
Daniil <128177413+beton18@users.noreply.github.com>
2024-04-21 20:19:37 +0300

    Add files via upload
    Query selects the count of customer IDs from the "customers" table and assigns it the alias "customers_count". It calculates the total number of customers present in the "customers" table.

---
+++ queries.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(customer_id) AS customers_count
+FROM customers;
+--Query selects the count of customer IDs from the "customers" table and assigns it the alias "customers_count". It calculates the total number of customers present in the "customers" table.
